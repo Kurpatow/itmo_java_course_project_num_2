9a51f3e6d8b1d541f9f3c8f8de8147df64b1cfca
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-11-20 23:25:34 +0300

    Course project num2: start

---
+++ src/kurpatow/itmo/java/course_project_num_2/Main.java
@@ -0,0 +1,7 @@
+package kurpatow.itmo.java.course_project_num_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/kurpatow/itmo/java/course_project_num_2/app_elements/Game.java
@@ -0,0 +1,4 @@
+package kurpatow.itmo.java.course_project_num_2.app_elements;
+
+public class Game {
+}
