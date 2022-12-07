a6ecc14fdde9c87338e42bb703f14f54d4cab032
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-12-07 15:11:25 +0300

    Course project num2: progress

--- src/kurpatow/itmo/java/course_work_2/app_elements/section/IndentInfo.java
+++ src/kurpatow/itmo/java/course_work_2/app_elements/section/IndentInfo.java
@@ -15,6 +15,84 @@ public final class IndentInfo {
     public final static String FOX_SECOND_REPLY = "Отправиться на поиски";
 
     //Абзац "Вернуться домой"
+    public final static String COME_BACK_HOME_HEADING = "Вернуться домой";
+    public final static String COME_BACK_HOME_TEXT = "Вернувшись домой, Лисёнок нашёл там Бельчонка.\n" +
+            "Оказалось, что Бельчонок пришёл на место встречи раньше и увидел там рой злобных пчел.\n" +
+            "Он поспешил предупредить об этом Лисёнка, но они разминулись.\n" +
+            "Наконец-то друзья нашли друг друга! Игра завершилась успехом";
 
+    //Абзац "Отправиться на поиски"
+    public final static String GO_TO_SEARCH_HEADING = "Отправиться на поиски";
+    public final static String GO_TO_SEARCH_TEXT = "Все лесные жители были заняты своими делами и не обращали " +
+            "внимания на Лисёнка и его проблему.\nНо вдруг кто нибудь видел Бельчонка... " +
+            "Лисёнок не знал, что ему делать. Помогите ему.";
+    public final static String GO_TO_SEARCH_FIRST_REPLY = "Попытаться разузнать о Бельчонке у лесных жителей";
+    public final static String GO_TO_SEARCH_SECOND_REPLY = "Искать Бельчонка в одиночку";
 
+    // Абзац "Разнюхать о Белочке"
+    public final static String ASK_FOREST_DWELLERS_HEADING = "Попытаться разузнать о Бельчонке у лесных жителей";
+    public final static String ASK_FOREST_DWELLERS_TEXT = "Пока Лисёнок принимал решение, лесные жители разошлись кто " +
+            "куда.\nОстались только Сова и Волк. Но у Совы бывают проблемы с памятью, а " +
+            "Волк может сильно разозлиться из-за расспросов. Кого выбрать?";
+    public final static String ASK_FOREST_DWELLERS_FIRST_REPLY = "Расспросить Сову";
+    public final static String ASK_FOREST_DWELLERS_SECOND_REPLY = "Расспросить Волка";
+
+    // Абзац "Ищем Белочку в одиночку"
+    public final static String SEARCH_ALONE_HEADING = "Искать Бельчонка в одиночку";
+    public final static String SEARCH_ALONE_TEXT = "Лисёнок слишком долго плутал по лесу в поисках друга и сам не " +
+            "заметил, как заблудился.\nТеперь его самого нужно искать. Игра завершилась неудачей";
+
+    // Абзац "Задаём вопросы Сове"
+    public final static String ASK_OWL_HEADING = "Расспросить Сову";
+    public final static String ASK_OWL_TEXT = "Сова долго не хотела говорить, но в итоге сказала, что видела " +
+            "испуганного Бельчонка,\nбежавшего вглубь леса. Все лесные жители знают, что в глубине леса опасно, " +
+            "если Бельчонок там, ему срочно нужна помощь.";
+    public final static String ASK_OWL_FIRST_REPLY = "Поверить Сове и отправиться вглубь леса";
+    public final static String ASK_OWL_SECOND_REPLY = "Сове не стоит верить";
+
+    // Абзац "Задаём вопросы Волку"
+    public final static String ASK_WOLF_HEADING = "Расспросить Волка";
+    public final static String ASK_WOLF_TEXT = "Волк оказался вполне дружелюбным, но помочь не смог.\n" +
+            "Лишь сказал, что маленькому лисенку не стоит бродить по лесу одному. И как теперь поступить?";
+    public final static String ASK_WOLF_FIRST_REPLY = "Волк прав";
+    public final static String ASK_WOLF_SECOND_REPLY = "Волку не стоит верить";
+
+    // Абзац "Доверяем Сове и отправляемся в лес"
+    public final static String TRUST_OWL_HEADING = "Поверить Сове и отправиться вглубь леса";
+    public final static String TRUST_OWL_TEXT = "В глубине леса Лисёнок встретил Медвежонка.\nЛенивый Медвежонок " +
+            "был готов рассказать все, что знает, если Лисёнок принесёт ему мёда.";
+    public final static String TRUST_OWL_FIRST_REPLY = "Нет, потрачено слишком много времени, нужно идти дальше";
+    public final static String TRUST_OWL_SECOND_REPLY = "Нужно воспользоваться шансом и раздобыть мёд";
+
+    // Абзац "Пользуемся шансом и берём мёд"
+    public final static String GET_HONEY_HEADING = "Нужно воспользоваться шансом и раздобыть мёд";
+    public final static String GET_HONEY_TEXT = "Лисёнок быстро нашёл мёд, но вокруг летал рой злобных пчел.\n" +
+            "Лисёнок всегда боялся пчёл, но и не найти друга он тоже боялся.";
+    public final static String GET_HONEY_FIRST_REPLY = "Подождать, вдруг пчёлы улетят";
+    public final static String GET_HONEY_SECOND_REPLY = "Нужно попытаться выкрасть мёд немедленно";
+
+    // Абзац "А может просто переждать?"
+    public final static String WAIT_BEES_HEADING = "Подождать, вдруг пчёлы улетят";
+    public final static String WAIT_BEES_TEXT = "Лисёнок подождал немного, и пчёлы разлетелись.\n" +
+            "Лисёнок без проблем набрал мёда. Вдруг он понял, что очень голоден. Что же делать?";
+    public final static String WAIT_BEES_FIRST_REPLY = "Поесть немного и передохнуть";
+    public final static String WAIT_BEES_SECOND_REPLY = "Скорее отнести мёд Медвежонку";
+
+    // Абзац "Срочно крадём мёд"
+    public final static String STEAL_HONEY_HEADING = "Нужно попытаться выкрасть мёд немедленно";
+    public final static String STEAL_HONEY_TEXT = "Это была не лучшая идея. Пчёлы покусали Лисёнка, " +
+            "теперь ему самому нужна помощь.\nИгра завершилась неудачей";
+
+    // Абзац "Перерыв на покушать"
+    public final static String RELAX_HEADING = "Поесть немного и передохнуть";
+    public final static String RELAX_TEXT = "Пока Лисёнок ел, злобные пчёлы вернулись и покусали его.\n" +
+            "Лисёнку нужна помощь, он не сможет продолжить поиски. Игра завершилась неудачей";
+
+    // Абзац "Срочно возвращаем Медведю мёд"
+    public final static String TAKE_HONEY_TO_BEAR_HEADING = "Скорее отнести мёд Медвежонку";
+    public final static String TAKE_HONEY_TO_BEAR_TEXT = "Довольный Медвежонок рассказал Лисёнку, что очень хорошо " +
+            "знает семью Белок и уверен, что Бельчонок никогда не пошёл бы в глубь леса.\nОн заверял Лисёнка, что " +
+            "Белки не попадают в неприятности, и что Совам нельзя верить, он также уговаривал Лисёнка вернуться домой.";
+    public final static String TAKE_HONEY_TO_BEAR_FIRST_REPLY = "Медвежонок ничего не знает, нужно продолжить поиски";
+    public final static String TAKE_HONEY_TO_BEAR_SECOND_REPLY = "Может быть он прав и Лисёнок просто паникует";
 }
